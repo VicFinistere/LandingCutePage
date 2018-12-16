8a6e1384bc845990ca382343bc9affe8e5ab07bc
zbelleton <zoebelleton@gmail.com>
2018-12-16 10:50:05 +0100

    without port

--- app.py
+++ app.py
@@ -5,6 +5,4 @@ Application launcher
 from page_app.app import app
 
 if __name__ == "__main__":
-    port = 8095
-    url = "http://127.0.0.1:{0}".format(port)
-    app.run(port=port, debug=False)
+    app.run()
